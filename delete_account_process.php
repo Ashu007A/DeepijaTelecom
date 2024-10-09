87f93123f5855ebb649dd1ad010f248c154ecfbc
Dtel <ashishrambsp0077@gmail.com>
2024-10-09 19:03:12 +0530

    Added Session timeout

--- delete_account_process.php
+++ delete_account_process.php
@@ -5,6 +5,20 @@ if (!isset($_SESSION['username'])) {
     exit();
 }
 
+// Timeout duration
+$timeout_duration = 60;
+
+// Check if the timeout variable is set and has passed
+if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity']) > $timeout_duration) {
+    session_unset();
+    session_destroy();
+    header("Location: login.php");
+    exit();
+}
+
+// Update last activity timestamp
+$_SESSION['last_activity'] = time();
+
 include "db_connect.php";
 
 $username = $_SESSION['username'];
